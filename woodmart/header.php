cce220be289ad53ddffc774f438a7ee09b4e0b3d
zte8131 <134517873+zte8131@users.noreply.github.com>
2023-07-11 13:37:02 +0300

    test

---
+++ woodmart/header.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The Header template for our theme
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
+	<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6489a26794cf5d49dc5da280/1h2sq4pbj';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+</head>
+
+<body <?php body_class(); ?>>
+	<?php if ( function_exists( 'wp_body_open' ) ) : ?>
+		<?php wp_body_open(); ?>
+	<?php endif; ?>
+
+	<?php do_action( 'woodmart_after_body_open' ); ?>
+
+	<div class="website-wrapper">
+		<?php if ( woodmart_needs_header() ) : ?>
+			<?php if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'header' ) ) : ?>
+				<header <?php woodmart_get_header_classes(); // phpcs:ignore ?>>
+					<?php whb_generate_header(); ?>
+				</header>
+			<?php endif ?>
+
+			<?php woodmart_page_top_part(); ?>
+		<?php endif ?>
